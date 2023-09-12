4684aba9844fcc7b7b5afb0bf5eceb7a488cad50
Sneha Choudhary <111838183+SnehaCh8@users.noreply.github.com>
2023-09-12 21:12:20 +1200

    Add files via upload

---
+++ forloop1.py
@@ -0,0 +1,9 @@
+# For loop
+
+user_input = int(input("Please enter your favorite number"))
+
+for i in range(user_input):
+    print ("Hello Sneha.....", i)
+# Trying to use the for loop but only getting the User input message when I run the code and not getting the print statement.
+
+# The code is correct and found out I was typing 0 in the option. So working after entering a positive number like 5 or 6 etc.
---
+++ test.py
@@ -0,0 +1,10 @@
+import datetime
+print ("Hello Sneha")
+launch_date = datetime.date(1991, 10, 8)
+print(launch_date)
+
+# input = raw_input("Please enter a test string:")
+
+#if len(input) < 8:
+ #   print("Your string is too short,")
+ #   print("Please enter a string with at least 6 character.")
