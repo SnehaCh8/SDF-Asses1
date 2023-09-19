3302bbe0569147068f9537151291cff47df0f1c6
Sneha Choudhary <111838183+SnehaCh8@users.noreply.github.com>
2023-09-19 21:34:06 +1200

    Add files via upload

---
+++ list.py
@@ -0,0 +1,7 @@
+# List
+
+# Helpdesk ticket menu
+helpdesk = ["0: Exit", "1: Submit helpdesk ticket", "2: Show all ticket", "3: Respond to ticket by number", 
+            "4: Re-open resolved ticket", "5: Display Ticket stats"]
+for helpdesk in helpdesk:
+    print(helpdesk)
