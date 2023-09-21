1447b171ee1e14e61fedbda988d9287317b6e60e
Sneha Choudhary <111838183+SnehaCh8@users.noreply.github.com>
2023-09-21 12:24:06 +1200

    Add files via upload

--- forloop1.py
+++ forloop1.py
@@ -1,7 +1,9 @@
-# For loop
+# For loop (You can limit your command to a certain number of times)
 
 user_input = int(input("Please enter your favorite number"))
 
+
+# This code should bring the below print message for any number that you will be entering. Such as 5 then it will bring  "Hello Sneha..... 1 till 4 in 5 lines(As the counting starts with 0 in coding)"
 for i in range(user_input):
     print ("Hello Sneha.....", i)
 # Trying to use the for loop but only getting the User input message when I run the code and not getting the print statement.
