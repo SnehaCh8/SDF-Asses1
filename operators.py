a3996dcfd328a88f1625aa05764b9a40ee673ff8
Sneha Choudhary <111838183+SnehaCh8@users.noreply.github.com>
2023-09-22 13:23:36 +1200

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,16 @@
+# comparison operator example -
+print("Welcome to my website")
+
+# != This is a not equal to sign. 
+student = input("Are you a student?\n")
+if student != "Yes":
+    print("Please enrol first")
+else:
+    print("Welcome dear student")
+
+# Logical operator
+x = 5
+# And logical operator where both should be True for it to return True result.
+print(x > 4 and x < 10)
+# Or logical operator - if one of the statement is true then it will return the True Boolean statement.
+print(x > 3 or x <1)
