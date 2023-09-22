6b11bea4a119acf15e89d0908a9d4706cec6a4bd
Sneha Choudhary <111838183+SnehaCh8@users.noreply.github.com>
2023-09-22 13:53:33 +1200

    Add files via upload

--- kivytest.py
+++ kivytest.py
@@ -2,6 +2,9 @@ from kivy.app import App
 from kivy. uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 
+# I think I had Kivy extension added in my VS code but still getting the error
+# "No module name Kivy" so not sure whats wrong.
+
 class root(App):
     def build(self):
         la=BoxLayout(orientation='vertical')
